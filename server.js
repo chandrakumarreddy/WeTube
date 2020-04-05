959c6fce78011c519ec5cccc5e159bf919514480
Chandra penugonda <chandrapenugonda655@gmail.com>
2020-04-05 21:31:19 +0530

    babel-watch config

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-console.log("object");
+import express from "express";
+const app = express();
+
+const PORT = 4000;
+
+const handleHome = (req, res) => res.send("Home page ");
+
+const handleProfile = (req, res) => res.send("Profile ");
+
+const handleListening = () => {
+  console.log(`server is listening at http://localhost:${PORT}`);
+};
+
+app.get("/", handleHome);
+app.get("/profile", handleProfile);
+
+app.listen(PORT, handleListening);
